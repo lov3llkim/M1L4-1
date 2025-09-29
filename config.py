7ff60833df7950e0065aae150baf162cb49d6dc6
lov3llkim <kirillkim007@mail.ru>
2025-09-29 19:38:48 +0300

    pokemon bot

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = ""
+token = "8135114608:AAGrqJc_av5uheR7bZREsI1zoGdsdjkQkHA"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot 
 from config import token
 
+
+
 from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
@@ -13,7 +15,26 @@ def go(message):
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
-
-
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
+@bot.message_handler(commands=['feed'])
+def feed(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        res = pok.feed()
+        bot.send_message(message.chat.id, res)
+    else:
+        bot.send_message(message.chat.id, "У вас нет покемона")
+     
 bot.infinity_polling(none_stop=True)
 
